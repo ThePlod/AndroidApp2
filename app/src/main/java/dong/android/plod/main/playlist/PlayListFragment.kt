3d74f2664175539738fe0546d5eb19d50a4e93f1
dongk00 <dddongk00@gmail.com>
2022-08-10 16:43:34 +0900

    binding / autoCleared applied

--- app/src/main/java/dong/android/plod/main/playlist/PlayListFragment.kt
+++ app/src/main/java/dong/android/plod/main/playlist/PlayListFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dong.android.plod.R
+import dong.android.plod.databinding.FragmentPlayListBinding
+import dong.android.plod.util.autoCleared
 
 class PlayListFragment : Fragment() {
+
+    private var binding by autoCleared<FragmentPlayListBinding>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_play_list, container, false)
+    ): View {
+        binding = FragmentPlayListBinding.inflate(layoutInflater)
+        return binding.root
     }
 
 }
